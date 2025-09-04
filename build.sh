c3c498849334fbe449e6ef18caada07af0cbc105
Heisphool <144183659+Heisphool@users.noreply.github.com>
2025-09-04 19:46:59 +0530

    build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 # exit on error
-set -o errexit
+set -e
 
 # Install necessary Python packages
 pip install -r requirements.txt
 
 # Install wkhtmltopdf and its dependencies
 apt-get update && apt-get install -y libxrender1 libfontconfig1 libxext6 wkhtmltopdf
+
